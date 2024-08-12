210f4a5550c5d21d76eecf4ef38a8e4f63fea789
phaiduykota <phaiduykota@gmail.com>
2024-08-12 23:56:27 +0700

    add Migrations

---
+++ FPTeeth-BE/Migrations/20240811082637_FUllName.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FPTeeth_BE.Migrations
+{
+    /// <inheritdoc />
+    public partial class FUllName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DortorName",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerName",
+                table: "Customers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Accounts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DortorName",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerName",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
