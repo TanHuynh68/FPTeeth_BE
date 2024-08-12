c624fea5910f88077ba47ddaf245b82db71a2f30
Khang Duy <duyktkse161141@fpt.edu.vn>
2024-08-12 15:36:57 +0700

    Update flow Clinic Owner

--- FPTeeth-BE/Extension/ServiceCollectionExtensions.cs
+++ FPTeeth-BE/Extension/ServiceCollectionExtensions.cs
@@ -11,6 +11,11 @@ namespace FPTeeth_BE.Extension
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             services.AddScoped<IAccountService, AccountService>();
             services.AddScoped<IClinicService, ClinicService>();
+            services.AddScoped<IBookingService, BookingService>();
+            services.AddScoped<IClinicServicesService, ClinicServicesService>();
+            services.AddScoped<IDoctorService, DoctorService>();
+            services.AddScoped<IServicesService, ServicesService>();
+            services.AddScoped<IWorkingTimeService, WorkingTimeService>();
         }
     }
 }
