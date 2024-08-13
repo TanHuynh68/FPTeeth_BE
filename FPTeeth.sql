1ca752384338249249486eccff14d8ee6033204f
hoangduong1910 <hoangtrieuduong1910@gmail.com>
2024-08-13 13:47:26 +0700

    add clinic to db

--- FPTeeth.sql
+++ FPTeeth.sql
@@ -13,3 +13,9 @@ VALUES
     ('1', 'phamquochuan@example.com', '1', NULL, '1', GETDATE(), GETDATE(), 2, N'Phạm Quốc Huân'),
     ('1', 'doctor1@example.com', 1, NULL, 1, GETDATE(), GETDATE(), 3, N'Nguyễn Minh Tâm'),
     ('1', 'doctor2@example.com', 1, NULL, 1, GETDATE(), GETDATE(), 3, N'Lê Văn Sơn'); 
+INSERT INTO [FPTeeth].[dbo].[Clinics] 
+    ([Name], [Description], [Address], [Image], [OwnerId], [CreateAt], [UpdateAt], [Status])
+VALUES
+    (N'Nha Khoa Kim', N'Nha khoa Kim – Chăm sóc sức khỏe răng miệng chuyên sâu.', N'123 Hoàng Văn Thụ, HCM City', NULL, 7, GETDATE(), GETDATE(), 1),
+    (N'Nha Khoa Việt Đức', N'Nha khoa Việt Đức – Dịch vụ nha khoa uy tín và chất lượng.', N'456 Nguyễn Trãi, Hà Nội', NULL, 11, GETDATE(), GETDATE(), 1),
+    (N'Nha Khoa Paris', N'Nha khoa Paris – Chuyên gia trong điều trị và thẩm mỹ răng miệng.', N'789 Lê Lợi, Đà Nẵng', NULL, 15, GETDATE(), GETDATE(), 1);
