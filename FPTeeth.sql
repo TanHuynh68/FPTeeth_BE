44cec4941405f1c98a1fc7d7be7e4e499918de5e
hoangduong1910 <hoangtrieuduong1910@gmail.com>
2024-08-13 13:43:54 +0700

    commit database

---
+++ FPTeeth.sql
@@ -0,0 +1,15 @@
+﻿INSERT INTO [FPTeeth].[dbo].[Accounts] 
+    ([Password], [Email], [Gender], [Image], [Status], [UpdateAt], [CreatedAt], [RoleId], [FullName])
+VALUES
+    ('1', 'hoangtrieuduong1910@gmail.com', '1', NULL, '1', GETDATE(), GETDATE(), 1, N'Hoàng Triệu Dương'),
+    ('1', 'nguyenvanan@example.com', '1', NULL, '1', GETDATE(), GETDATE(), 2, N'Nguyễn Văn An'),
+    ('1', 'tranbinhtan@example.com', '1', NULL, '1', GETDATE(), GETDATE(), 3, N'Trần Bình Tân'),
+    ('1', 'phamminhcuong@example.com', '1', NULL, '1', GETDATE(), GETDATE(), 4, N'Phạm Minh Cường'),
+    ('1', 'lequocdung@example.com', '1', NULL, '1', GETDATE(), GETDATE(), 1, N'Lê Quốc Dũng'),
+    ('1', 'doanphulan@example.com', '1', NULL, '1', GETDATE(), GETDATE(), 2, N'Đoàn Phú Lân'),
+    ('1', 'vuongtrongPhúc@example.com', '1', NULL, '1', GETDATE(), GETDATE(), 3, N'Vương Trọng Phúc'),
+    ('1', 'buiducgiang@example.com', '1', NULL, '1', GETDATE(), GETDATE(), 4, N'Bùi Đức Giang'),
+    ('1', 'nguyenkhanhh@example.com', '1', NULL, '1', GETDATE(), GETDATE(), 1, N'Nguyễn Khánh Hòa'),
+    ('1', 'phamquochuan@example.com', '1', NULL, '1', GETDATE(), GETDATE(), 2, N'Phạm Quốc Huân'),
+    ('1', 'doctor1@example.com', 1, NULL, 1, GETDATE(), GETDATE(), 3, N'Nguyễn Minh Tâm'),
+    ('1', 'doctor2@example.com', 1, NULL, 1, GETDATE(), GETDATE(), 3, N'Lê Văn Sơn'); 
